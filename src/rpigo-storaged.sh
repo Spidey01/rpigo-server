2f5f4fc7ee7e0f8d3dc58cb61553bfd0b20d6c74
Terry M. Poulin <BigBoss1964@gmail.com>
2014-11-24 21:18:41 +0000

    Include vfat in uid/gid var names; add dmask/fmask too.

--- src/rpigo-storaged.sh
+++ src/rpigo-storaged.sh
@@ -84,8 +84,8 @@ mount_device() {
         #
         # these mount options have dynamic defaults if not in the config.
         #
-        [ -z "$storage_mount_uid" ] && storage_mount_uid=$(id -u)
-        [ -z "$storage_mount_gid" ] && storage_mount_gid=$(id -g)
+        [ -z "$storage_mount_vfat_uid" ] && storage_mount_vfat_uid=$(id -u)
+        [ -z "$storage_mount_vfat_gid" ] && storage_mount_vfat_gid=$(id -g)
 
         #
         # Handle mount options.
@@ -94,12 +94,14 @@ mount_device() {
         [ -n "$storage_mount_noexec" ]  && volume_options="${volume_options},noexec"
         [ -n "$storage_mount_nodev" ]   && volume_options="${volume_options},nodev"
         [ -n "$storage_mount_nosuid" ]  && volume_options="${volume_options},nosuid"
-        # Humbug, these work on FAT and some other stuff but bomb out on ext.
-        if [ "$volume_format" = "vfat" ]; then
-            [ -n "$storage_mount_uid" ] && volume_options="${volume_options},uid=$storage_mount_uid"
-            [ -n "$storage_mount_gid" ] && volume_options="${volume_options},gid=$storage_mount_gid"
-        fi
         [ -n "$volume_options" ]        && volume_options="-o defaults${volume_options}"
+
+        if [ "$volume_format" = "vfat" ]; then
+            [ -n "$storage_mount_vfat_uid" ]    && volume_options="${volume_options},uid=$storage_mount_vfat_uid"
+            [ -n "$storage_mount_vfat_gid" ]    && volume_options="${volume_options},gid=$storage_mount_vfat_gid"
+            [ -n "$storage_mount_vfat_dmask" ]  && volume_options="${volume_options},dmask=$storage_mount_vfat_dmask"
+            [ -n "$storage_mount_vfat_fmask" ]  && volume_options="${volume_options},fmask=$storage_mount_vfat_fmask"
+        fi
 
 
         #
