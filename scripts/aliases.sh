c3fab6dbec2562ef1ea6e3dab3ef6668e70bc97a
Terry M. Poulin <BigBoss1964@gmail.com>
2015-06-11 19:47:30 +0000

    Fix C function in aliases.sh

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -2,5 +2,5 @@ alias L="cd /var/log/rpigo"
 alias S="cd /home/pi/rpigo-server/"
 alias Q="cd /var/spool/rpigo/queue"
 C() { # yeah, I'm really functional :P.
-    (S && echo `pwd` == ./scripts/send.sh -r $*)
+    (S && sudo ./scripts/send.sh -r $*)
 }
